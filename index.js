29eeab678f081c822dad6e65de72e0f85646adb1
hanifizzapratama <hanifheinrich@gmail.com>
2022-03-24 18:09:25 +0700

    update hanif

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ app.get("/login/halamanberanda", function (request, response) {
   response.send("Anda telah berhasil login");
 });
 
+//Bagian Hanif Izza Pratama 9-13
+//9 Dosen dapat menambah CPMK mata kuliah
+app.get("/", function (request, response) {
+  response.send("Ini adalah halaman 9");
+});
+
+//10 Dosen dapat mengubah CPMK mata kuliah
+app.get("/", function (request, response) {
+  response.send("Ini adalah halaman 10");
+});
+
+//11 Dosen dapat menghapus CPMK mata kuliah
+app.get("/", function (request, response) {
+  response.send("Ini adalah halaman 11");
+});
+
+//12 Dosen dapat menambah referensi
+app.get("/", function (request, response) {
+  response.send("Ini adalah halaman 12");
+});
+
+//13 Dosen dapat menambah referensi
+app.get("/", function (request, response) {
+  response.send("Ini adalah halaman 13");
+});
+
 app.listen(3000, function () {
   console.log("server sedang berjalan");
 });
