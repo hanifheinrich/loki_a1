fbf00f49804f9630b19ad9982ba2d6adbb5d78ce
hanifizzapratama <hanifheinrich@gmail.com>
2022-04-09 22:20:15 +0700

    routerbaru

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const express = require("express");
+const routerHanif = require("./routerkerjaanhanif");
 const res = require("express/lib/response");
 
 const app = express();
@@ -20,50 +21,7 @@ app.get("/login/halamanberanda", function (request, response) {
   response.send("Anda telah berhasil login");
 });
 
-//Bagian Hanif Izza Pratama 9-13
-app
-  .route("/cpmk")
-  //9 Dosen dapat menambah CPMK mata kuliah
-  .post(function (request, response) {
-    let obj = {
-      message: "Anda berhasil menambahkan Capaian Pembelajaran Mata Kuliah",
-      code_eror: 0,
-    };
-    response.json(obj);
-  })
-  //10 Dosen dapat mengubah CPMK mata kuliah
-  .put(function (request, response) {
-    const id = request.params.cpmk;
-    response.send(id);
-  })
-  //11 Dosen dapat menghapus CPMK mata kuliah
-  .delete(function (request, response) {
-    let obj = {
-      message: "Anda berhasil menghapus Capaian Pembelajaran Mata Kuliah",
-      code_eror: 0,
-    };
-    response.json(obj);
-  });
-
-//12 Dosen dapat menambah referensi
-app.get("/addreferensi", function (request, response) {
-  //responsejson
-  const mahasiswa = {
-    nim: 2011521023,
-    nama: "HanifIzzaPratama",
-    umur: 19,
-  };
-  response.json(mahasiswa);
-});
-
-//13 Dosen dapat mengubah referensi
-app.get("/editrefernsi", function (request, response) {
-  let obj = {
-    message: "Anda berhasil mengubah referensi",
-    code_eror: 0,
-  };
-});
-
+app.use(routerHanif);
 app.listen(3000, function () {
   console.log("server sedang berjalan");
 });
---
+++ routerkerjaanhanif.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hanif = express.Hanif();
+//Bagian Hanif Izza Pratama 9-13
+hanif
+  .route("/cpmk")
+  //9 Dosen dapat menambah CPMK mata kuliah
+  .post(function (request, response) {
+    let obj = {
+      message: "Anda berhasil menambahkan Capaian Pembelajaran Mata Kuliah",
+      code_eror: 0,
+    };
+    response.json(obj);
+  })
+  //10 Dosen dapat mengubah CPMK mata kuliah
+  .put(function (request, response) {
+    const id = request.params.cpmk;
+    response.send(id);
+  })
+  //11 Dosen dapat menghapus CPMK mata kuliah
+  .delete(function (request, response) {
+    let obj = {
+      message: "Anda berhasil menghapus Capaian Pembelajaran Mata Kuliah",
+      code_eror: 0,
+    };
+    response.json(obj);
+  });
+
+//12 Dosen dapat menambah referensi
+hanif
+  .route("/referensi")
+  .post(function (request, response) {
+    //responsejson
+    const mahasiswa = {
+      nim: 2011521023,
+      nama: "HanifIzzaPratama",
+      umur: 19,
+    };
+    response.json(mahasiswa);
+  })
+
+  //13 Dosen dapat mengubah referensi
+  .put(function (request, response) {
+    let obj = {
+      message: "Anda berhasil mengubah referensi",
+      code_eror: 0,
+    };
+  });
+
+module.export = hanif;
