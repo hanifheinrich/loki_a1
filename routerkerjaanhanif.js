42a99dda8f20d77bb3c9ad07bd0c26a78d2af13f
hanifizzapratama <hanifheinrich@gmail.com>
2022-04-10 16:31:11 +0700

    method delete

--- routerkerjaanhanif.js
+++ routerkerjaanhanif.js
@@ -5,30 +5,34 @@ hanif.use(express.json()); // for parsing application/json
 hanif.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //data.json
-let mahasiswa = [{ nama: "Hanif Izza Pratama", nim: 2011521023, umur: 19 }];
+let cpmks = [
+  { course_plan_detail_id: 001, description: "Mahasiswa mampu menjelaskan konsep dasar dan komponen aplikasi berbasis web (CP-1, CP-2)" },
+  { course_plan_detail_id: 002, description: "Mahasiswa mampu ngoding" },
+];
 let referensi = [{ course_plan_id: 001, tittle: "Beginning PHP 5.3", author: "Matt Doyle", publisher: "Wrox", year: 2009, decription: "" }];
 
 //Bagian Hanif Izza Pratama 9-13
-hanif
-  .route("/cpmk")
-  //9 Dosen dapat menambah CPMK mata kuliah
-  .post(function (request, response) {
-    mahasiswa.push(request.body);
-    response.send(mahasiswa);
-  })
-  //10 Dosen dapat mengubah CPMK mata kuliah
-  .put(function (request, response) {
-    const id = request.params.cpmk;
-    response.send(id);
-  })
-  //11 Dosen dapat menghapus CPMK mata kuliah
-  .delete(function (request, response) {
-    let obj = {
-      message: "Anda berhasil menghapus Capaian Pembelajaran Mata Kuliah",
-      code_eror: 0,
-    };
-    response.json(obj);
+//9 Dosen dapat menambah CPMK mata kuliah
+hanif.post("/cpmk", function (request, response) {
+  course_plan_detail_outcomes.push(request.body);
+  response.send(course_plan_detail_outcomes);
+});
+//10 Dosen dapat mengubah CPMK mata kuliah
+hanif.put("/cpmk/:course_plan_detail_id", function (request, response) {
+  const course_plan_detail_id = request.params.course_plan_detail_id;
+  cpmks.filter((cpmk) => {
+    if (cpmk.course_plan_detail_id == course_plan_detail_id) cpmk.course_plan_detail_id = course_plan_detail_id;
+    cpmk.description = request.body.description;
+    return cpmk;
   });
+  response.json(cpmks);
+});
+//11 Dosen dapat menghapus CPMK mata kuliah
+hanif.delete("/cpmk/:deleteId", function (request, response) {
+  let id = request.params.deleteId;
+  cpmks = cpmks.filter((cpmk) => cpmk.course_plan_detail_id != id);
+  response.send(cpmks);
+});
 
 //12 Dosen dapat menambah referensi
 hanif.post("/referensi", function (request, response) {
