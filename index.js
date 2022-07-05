4ddda64e47c7fe3fe283d1d454098275ed745576
hanifizzapratama <hanifheinrich@gmail.com>
2022-07-05 18:52:42 +0700

    a
    a

--- index.js
+++ index.js
@@ -4,27 +4,18 @@ const express = require("express");
 const res = require("express/lib/response");
 //impor modul hanif
 const hanifRouter = require("./router/routerkerjaanhanif");
-const user_controller = require("./router/user");
-import session from "express-session";
 const app = express();
-
-app.use(
-  session({
-    secret: "ini adalah kode secret",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60 * 60 * 1000 },
-  })
-);
 
 //set template engine
 app.set("view engine", "ejs");
 
-app.get("/", user_controller.login);
-
 app.use(hanifRouter);
 app.listen(3000, function () {
   console.log("server sedang berjalan");
+});
+
+app.use("/", (request, response) => {
+  response.render("login.ejs");
 });
 
 app.use("/", (request, response) => {
